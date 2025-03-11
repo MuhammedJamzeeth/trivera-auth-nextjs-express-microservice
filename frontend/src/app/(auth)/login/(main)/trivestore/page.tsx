ce5716f03cb3affdc5c7e37a45c9a903becc1d85
Mohamad Siyam <siyam_2020679@fot.ruh.ac.lk>
2025-03-11 16:10:59 +0530

    update trivestore
    added login form component

--- frontend/src/app/(auth)/login/(main)/trivestore/page.tsx
+++ frontend/src/app/(auth)/login/(main)/trivestore/page.tsx
@@ -1,16 +1,18 @@
-
-import {LoginImageSideData} from "../../../../../../constans";
 import OnBoarding from "@/components/OnBoarding";
+import {LoginImageSideData} from "../../../../../../constans";
 import {LOGINPAGES} from "@/types/index.types";
+import LoginForm from "@/components/LoginForm";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
 
 
 const page = () => {
     const loginData = LoginImageSideData[1];
+
     return (
-
-
         <div>
-            <div className="flex items-center justify-between h-screen">
+            <div className="flex items-center flex- h-screen space-x-12 bg-white">
                 <OnBoarding  type={LOGINPAGES.TRIVESTORE} image={loginData.image} amount={loginData.card.amount}
                             icon={loginData.card.icon}
                             heading={
@@ -24,12 +26,17 @@ const page = () => {
                                     <span className=" text-[#05918A] font-bold"> + 1,0% </span>
                                      <span className="text-[#747681] text-sm">from last week</span>
 
-                                 </div>}/>
-                <div>
-                    dummy
+                                 </div>}
+                />
+                {/*Login Form Component*/}
+                <div className="flex-1 h-full flex justify-center items-center bg-white">
+                    <LoginForm
+                        logo={<Image src="/svg/Trivestore-38 1.svg" alt="icon" width={91} height={57.296}/>}
+                        nameLogo={<Image src="/png/Trivestoreheading.png" className="mt-2" alt="Trivemart heading.svg" width={174} height={45}/>}
+                        link={<Link href="/register/trivestore" className="text-[#8A608B]" >Register </Link>}
+                        type={LOGINPAGES.TRIVESTORE}
+                    />
                 </div>
-
-
             </div>
         </div>
   )
